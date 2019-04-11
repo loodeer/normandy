cd10fc333aef4923b289b8113039389df0bb9ba0
luzuheng <751564347@qq.com>
2019-04-11 16:57:12 +0800

    facade init

---
+++ normandy-service-facade/src/main/java/com/loodeer/normandy/facade/NormandyServiceFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.loodeer.normandy.facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormandyServiceFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormandyServiceFacadeApplication.class, args);
+	}
+
+}
