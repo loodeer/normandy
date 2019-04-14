911f4ee0d5f14d4a21d0c914f76a92fd9c32901d
luzuheng <751564347@qq.com>
2019-04-15 00:52:54 +0800

    fix bug: tradeService missing consumer

--- normandy-controller/src/main/java/com/loodeer/normandy/controller/trade/impl/TradeControllerImpl.java
+++ normandy-controller/src/main/java/com/loodeer/normandy/controller/trade/impl/TradeControllerImpl.java
@@ -3,11 +3,13 @@ package com.loodeer.normandy.controller.trade.impl;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.loodeer.normandy.controller.trade.TradeController;
 import com.loodeer.normandy.facade.trade.TradeService;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author luzuheng
  * @date 2019-04-11 21:16
  */
+@RestController
 public class TradeControllerImpl implements TradeController {
 
     @Reference(version = "1.0.0")
